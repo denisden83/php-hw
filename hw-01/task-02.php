824935970101327421a0922e84853ee2e2426b28
denisden83 <denisden83@gmail.com>
2017-07-15 23:06:01 +0200

    #02.master second ok

---
+++ hw-01/task-02.php
@@ -0,0 +1,7 @@
+<?php
+$total_amount = 80;
+$felt_tip_pen = 23;
+$pencil = 40;
+$paints = $total_amount - $felt_tip_pen - $pencil;
+echo "На школьной выставке $total_amount рисунков. $felt_tip_pen из них выполнены фломастерами, " .
+    $pencil . " карандашами, а остальные - красками.";
