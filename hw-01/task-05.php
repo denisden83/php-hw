391fc1e713b5abd6323060b42c97276069854109
denisden83 <denisden83@gmail.com>
2017-07-16 16:01:13 +0200

    #05.master task five

--- hw-01/task-05.php
+++ hw-01/task-05.php
@@ -1,7 +1,15 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: den
- * Date: 16/07/2017
- * Time: 02:00
- */
+$day = rand(0, 10);
+$day = 0; //почему 0 поподает в первый case?
+echo "$day <br />\n";
+
+switch ($day) {
+    case ($day >= 1 && $day <= 5):
+        echo "Это рабочий день " . gettype($day);
+        break;
+    case ($day == 6 || $day == 7):
+        echo "Это выходной день";
+        break;
+    default:
+        echo "There's no such a day";
+}
