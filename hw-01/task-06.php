b80dc094dcb2a7a4f12d3cdc1c4e25db0cef0a8e
denisden83 <denisden83@gmail.com>
2017-07-16 18:24:53 +0200

    #06.master six

---
+++ hw-01/task-06.php
@@ -0,0 +1,39 @@
+<?php
+$bmw = [
+    "model"=>"X5",
+    "speed"=>120,
+    "doors"=>5,
+    "year"=>2015
+];
+$toyota['model'] = "RAV4";
+$toyota['speed'] = 220;
+$toyota['doors'] = 5;
+$toyota['year'] = 2017;
+
+$opel = [
+    "model"=>"FOCUS",
+    "speed"=>200,
+    "doors"=>5,
+    "year"=>2014
+];
+
+$cars = ["bmw"=>$bmw, "toyota"=>$toyota, "opel"=>$opel];
+
+//var_dump($cars);
+foreach ($cars as $make => $specs) {
+    echo "CAR $make <br />\n";
+    foreach ($specs as $key => $value) {
+        echo "$key $value; ";
+    }
+    echo "<br />\n";
+
+}
+//$arrlength = count($cars);
+//for ($i = 0; $arrlength--; $i++) {
+//    echo $cars[$i] . "<br />\n";
+//
+//    foreach ($cars[$i] as $key=>$value) {
+//        echo $value . " ";
+//    }
+//    echo "<br />\n";
+//}
