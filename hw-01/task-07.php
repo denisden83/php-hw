11a4be37d640e630d770e6bb506b7d1da9ddac20
denisden83 <denisden83@gmail.com>
2017-07-17 18:32:51 +0200

    #07.master added css

--- hw-01/task-07.php
+++ hw-01/task-07.php
@@ -1,15 +1,31 @@
-<?php
-echo "<table >\n<tr>";
+<style>
+    table, th, td {
+        border: 1px solid black;
+        text-align: center;
+    }
+    td {
 
+    }
+    table {
+        border-collapse: collapse;
+    }
+
+</style>
+<?php
+echo "<table>\n<tr><td></td>";
+for ($x = 11; --$x;) {
+    echo "<th>$x</th>";
+}
+echo "</tr>\n<tr>";
 for ($i = 11; --$i;) {
-//    echo " <td>$i</td>";
+    echo "<th>$i</th>";
     for ($j = 11; --$j;) {
         if ($i % 2 == 0 && $j % 2 == 0) {
             echo "<td>(" . ($i * $j) . ")</td>";
         } elseif ($i % 2 == 1 && $j % 2 == 1) {
             echo "<td>[" . ($i * $j) . "]</td>";
         } else {
-            echo "<td>&nbsp" . ($i * $j) . "</td>";
+            echo "<td>" . ($i * $j) . "</td>";
         }
     }
     echo ($i != 1) ? "</tr>\n<tr>" : "</tr>\n";
